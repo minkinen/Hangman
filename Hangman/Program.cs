c762beeed737c06ae37e1a45947e532f6aaffcf8
minkinen <minkinen@yahoo.com>
2022-03-29 22:37:16 +0200

    Added char[] hiddenLetters and filled it with _

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace Hangman
+﻿namespace Hangman
 {
     class Program
     {
@@ -14,15 +12,32 @@ namespace Hangman
             int indexNumber = randomNumber.Next(0, words.Length);
             string hiddenWord = words[indexNumber];
 
+            // The correct letters inside a char array. 
+            int hiddenWordLength = words[indexNumber].Length;
+            char[] hiddenLetters = new char[hiddenWordLength];
+
+            // Unrevealed letters represented by a lower dash(_).
+            startCondition(hiddenLetters);
+
+
 
 
 
 
             // Display
+            Console.WriteLine(hiddenLetters);
             Console.WriteLine(hiddenWord);
             Console.ReadLine();
 
 
+        }
+
+        static void startCondition(char[] allHiddenLetters)
+        {
+            for (int i = 0; i < allHiddenLetters.Length; i++)
+            {
+                allHiddenLetters[i] = '_';
+            }
         }
     }
 }
@@ -35,8 +50,7 @@ namespace Hangman
 // Guess for the whole word. The player type in a word he/she thinks is the word. If the guess is correct player wins the game and the whole word is revealed. If the word is incorrect nothing should get reveale
 // If the player guesses the same letter twice, the program will not consume a guess.
 // The incorrect letters the player has guessed, should be put inside a StringBuilder and presented to the player after each guess
-// The correct letters should be put inside a char array. Unrevealed letters need to be represented by a lower dash(_).
+// (3) The correct letters should be put inside a char array. Unrevealed letters need to be represented by a lower dash(_).
 
 // (optional) You unit tests need to have at least 50% coverage.
 // (optional) Read in the words from a text file with Comma-separated values and then store them in an array or list of Strings.
-
