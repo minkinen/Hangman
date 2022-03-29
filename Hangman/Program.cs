5486b459354c60b399ccc7f96f0eac6e0ee2ddf1
minkinen <minkinen@yahoo.com>
2022-03-29 10:41:35 +0200

    The secret word randomly chosen from an array of Strings

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -6,19 +6,25 @@ namespace Hangman
 {
     static void Main()
     {
-            // (1) words in an array of strings
+            // (1) Words in an array of strings
             string[] words = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" };
+
+            // (2) The secret word should be randomly chosen from an array of Strings
+            Random selectedWord = new Random();
+            int selectedWordIndex = selectedWord.Next(0, words.Length);
+            Console.WriteLine($"{words[selectedWordIndex]}");
+            Console.ReadLine();
+        }
     }
 }
-}
 
 
-//  10 guesses count
-// guess letter
-// guess word
-// track letters that are used and don't count them again if guessed
-// wrong guessed letters (& words?) presented in a stringbilder after each guess
-// The correct letters should be put inside a char array. Unrevealed letters need to be represented by a lower dash(_)
+// The player has 10 guesses to complete the word before losing the game. The player can make two type of guesses; letter or word.
+// Guess for a specific letter. If player guess a letter that occurs in the word, the program should update by inserting the letter in the correct position(s).
+// Guess for the whole word. The player type in a word he/she thinks is the word. If the guess is correct player wins the game and the whole word is revealed. If the word is incorrect nothing should get reveale
+// If the player guesses the same letter twice, the program will not consume a guess.
+// The incorrect letters the player has guessed, should be put inside a StringBuilder and presented to the player after each guess
+// The correct letters should be put inside a char array. Unrevealed letters need to be represented by a lower dash(_).
 
 // (optional) You unit tests need to have at least 50% coverage.
 // (optional) Read in the words from a text file with Comma-separated values and then store them in an array or list of Strings.
