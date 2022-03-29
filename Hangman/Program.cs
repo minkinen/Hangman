570e749a2b32343c43aa4ae9a8f5f0d1eabfbf29
minkinen <minkinen@yahoo.com>
2022-03-29 09:50:50 +0200

    Add project files.

---
+++ Hangman/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Hangman 
+{ 
+    class Program
+{
+    static void Main()
+    {
+            // (1) words in an array of strings
+            string[] words = { "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY" };
+    }
+}
+}
+
+
+//  10 guesses count
+// guess letter
+// guess word
+// track letters that are used and don't count them again if guessed
+// wrong guessed letters (& words?) presented in a stringbilder after each guess
+// The correct letters should be put inside a char array. Unrevealed letters need to be represented by a lower dash(_)
+
+// (optional) You unit tests need to have at least 50% coverage.
+// (optional) Read in the words from a text file with Comma-separated values and then store them in an array or list of Strings.
+
+
+
