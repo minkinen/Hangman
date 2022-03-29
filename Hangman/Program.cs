0c0998bd74f4509f721302fdcba60d709352bed9
minkinen <minkinen@yahoo.com>
2022-03-29 23:07:52 +0200

    Guess Letter function added.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -19,6 +19,13 @@
             // Unrevealed letters represented by a lower dash(_).
             startCondition(hiddenLetters);
 
+            // Guess for a specific letter.
+            char inputGuess = char.Parse(Console.ReadLine().ToUpper());
+
+            // If player guess a letter that occurs in the word, the program update by inserting the letter in the correct position(s).
+            GuessLetter(inputGuess, hiddenWord, hiddenLetters);
+
+
 
 
 
@@ -32,6 +39,16 @@
 
         }
 
+        static void GuessLetter(char inputGuess, string hiddenWord, char[] hiddenLetters)
+        {
+            int i = 0;
+            foreach (var hiddenWordLetter in hiddenWord)
+            {
+                i++;
+                if (inputGuess == hiddenWordLetter)
+                    hiddenLetters[i - 1] = inputGuess;
+            }
+        }
         static void startCondition(char[] allHiddenLetters)
         {
             for (int i = 0; i < allHiddenLetters.Length; i++)
@@ -46,7 +63,7 @@
 // (1) Words in an array of strings
 // (2) The secret word should be randomly chosen from an array of Strings
 // The player has 10 guesses to complete the word before losing the game. The player can make two type of guesses; letter or word.
-// Guess for a specific letter. If player guess a letter that occurs in the word, the program should update by inserting the letter in the correct position(s).
+// (4) Guess for a specific letter. If player guess a letter that occurs in the word, the program should update by inserting the letter in the correct position(s).
 // Guess for the whole word. The player type in a word he/she thinks is the word. If the guess is correct player wins the game and the whole word is revealed. If the word is incorrect nothing should get reveale
 // If the player guesses the same letter twice, the program will not consume a guess.
 // The incorrect letters the player has guessed, should be put inside a StringBuilder and presented to the player after each guess
